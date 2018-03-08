66ebc8ba0af8e14d93097c1b6196c1c0ab1a3162
赵慧鑫 <32922054+ZhaoHuiXin@users.noreply.github.com>
2018-03-08 19:37:54 +0800

    Update cinema.py

--- FPproject/api/cinema.py
+++ FPproject/api/cinema.py
@@ -7,15 +7,13 @@ from FPproject.helper.code import Code
 from FPproject.extensions.validator import Validator
 from FPproject.models.play import Play
 from FPproject.models.movie import Movie
-# 为了简化return jsonify。。。自定义了ApiView,它继承自FlaskView，
-# 改写了其中构造response的部分，使其能对将要制造response对象的数据进行自定义格式化
+
 class CinemaView(ApiView):
 
     def all(self):
         cinemas = Cinema.query.all()
         return cinemas
 
-    # 传id返回具体详细信息
     @Validator(cid=int)
     def get(self):
         cid = request.params['cid']
@@ -24,8 +22,7 @@ class CinemaView(ApiView):
             return Code.cinema_does_not_exist, request.params
 
         return cinema
-    # 为了能对客户端传递的参数进行类型过滤，并返回些许提示
-    # 定义了类装饰器Validator
+
     @Validator(cid=int)
     def halls(self):
         # cid = request.args['cid']
