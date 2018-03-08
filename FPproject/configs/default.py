bf76b9c13c5b23f835f247a06cfa8168664f2862
赵慧鑫 <32922054+ZhaoHuiXin@users.noreply.github.com>
2018-03-08 19:50:39 +0800

    Update default.py

--- FPproject/configs/default.py
+++ FPproject/configs/default.py
@@ -4,7 +4,7 @@ class DefaultConfig(object):
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))  # 前面的目录上升两级
 
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:666666@localhost/FPproject'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@host/FPproject'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_ECHO = True
 
