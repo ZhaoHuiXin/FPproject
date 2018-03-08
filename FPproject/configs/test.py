10cf8d33c11ea9d71e967aa837fd02dc741ab4d3
赵慧鑫 <32922054+ZhaoHuiXin@users.noreply.github.com>
2018-03-08 19:49:05 +0800

    Update test.py

--- FPproject/configs/test.py
+++ FPproject/configs/test.py
@@ -3,4 +3,5 @@ class TestConfig(DefaultConfig):
     TESTING = True
     JSON_SORT_KEYS = False  # 稍微提高性能，
     SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite://' # 不写路径表示生成在进程的内存里，没有文件，进程结束就没了
+    SQLALCHEMY_DATABASE_URI = 'sqlite://' 
+    # 生成在进程的内存里，没有文件，进程结束就没了
